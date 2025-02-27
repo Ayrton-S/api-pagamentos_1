27b622973d72968c14b34d32b52a8c5ba3c8925e
Ayrtom-S <ayrtoncarlos@gmail.com>
2025-02-26 21:05:09 -0400

    2. Criação das entidades e mapeamento

--- src/main/java/edu/uea/dsw/api_pagamentos/model/Lancamento.java
+++ src/main/java/edu/uea/dsw/api_pagamentos/model/Lancamento.java
@@ -1,19 +1,55 @@
 package edu.uea.dsw.api_pagamentos.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 //asfddh
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-
+@AllArgsConstructor
+@Entity
+@Table(name = "lancamento")
 public class Lancamento {
-    private Long codigo;
-    private String descricao;
-    private String dataVencimento;
-    private String dataPagamento;
-    private Double valor;
-    private String observacao;
-    private TipoLancamento tipo;
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long codigo;
+
+   @NotNull
+   private String descricao;
+
+   @NotNull
+   private BigDecimal valor;
+
+   @NotNull
+   private LocalDate dataVencimento;
+
+   private LocalDate dataPagamento;
+
+   private String observacao;
+
+   @NotNull
+   @Enumerated(EnumType.STRING)
+   private TipoLancamento tipo;
+
+   @ManyToOne
+   @JoinColumn(name = "categoria_codigo")
+   private Categoria categoria;
+
+   @ManyToOne
+   @JoinColumn(name = "pessoa_codigo")
+   private Pessoa pessoa;
 }
